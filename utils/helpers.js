5232b53af38ea4cb6cb3549065ed5b7d2aecf449
markciubal <mark.ciubal@gmail.com>
2023-06-26 19:12:05 -0700

    Working on browse tasks.

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = {
   get_emoji: () => {
     const randomNum = Math.random();
@@ -11,4 +13,9 @@ module.exports = {
 
     return `<span for="img" aria-label="book">${book}</span>`;
   },
+  svg : function(iconName) {
+      // points to my icons folder
+      let path = '/public/images/' + iconName + '.svg';
+      return path;
+    }
 };
