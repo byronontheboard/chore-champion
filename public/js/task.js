07f344b044bfe4ceb060a3cf0f8f3ebfecdcb81a
bjs <127002178+benstorlie@users.noreply.github.com>
2023-06-29 20:12:42 -0700

    aligned id queries

--- public/js/task.js
+++ public/js/task.js
@@ -5,29 +5,34 @@ const loginFormHandler = async (event) => {
   // TODO: Add a comment describing the functionality of these expressions
   const title = document.querySelector('#title-task').value.trim();
   const body = document.querySelector('#body-task').value.trim();
-  const due_date = new Date(document.querySelector('#datetime').value).toISOString().slice(0, 19).replace('T', ' ');
-  const minutes = document.querySelector('#minutes');
-  const points = document.querySelector('#points')
-  const priority = document.querySelector('#priority');
-  const complete_date = document.querySelector('#complete_date');
+  const due_date = new Date(document.querySelector('#due_date').value);
+  const minutes = document.querySelector('#minutes').value;
+  const points = document.querySelector('#points').value
+  const priority = document.querySelector('#priority').value;
+  const complete_date = document.querySelector('#complete_date').value;
 
   // console.log(due_date);
   if (title && body) {
     // const due_date = '2023-06-27 12:00:00'
-    alert(due_date);
     // TODO: Add a comment describing the functionality of this expression
     const response = await fetch('/api/tasks/create', {
       method: 'POST',
-      body: JSON.stringify({ title, body, due_date, priority, minutes, points, complete_date }),
+      body: JSON.stringify({ 
+        title,
+        body, 
+        priority,
+        due_date,
+        minutes, 
+        points
+      }),
       headers: { 'Content-Type': 'application/json' },
     });
-    alert(due_date);
 
     if (response.ok) {
       alert('Successfully tasked!');
       document.location.replace('/');
     } else {
-      alert('Failed to log in');
+      alert('Task failed to create.');
     }
   }
 };
